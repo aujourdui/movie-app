2c3ee08fd018d59093217cc3a4e1b997ef2c6e1e
kyosuke ito <kyosuke.ito@gmail.com>
2021-11-24 09:45:39 -0800

    add insert photo dynamiclly function

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 const apiKey = "67a465f165043b63372ea02407bc5582";
-// const randomId = Math.floor(Math.random() * 1000)
-// `https://api.themoviedb.org/3/movie/550?api_key=${apiKey}`
-
-// `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&page=1&query=
-//"movie"&poster_path="string"`
-
-//`https://api.themoviedb.org/3/genre/movie/list?api_key=<<api_key>>&language=en-US`
 
 const fetchMovieGenre = async () => {
   const response = await fetch(
@@ -26,7 +19,6 @@ const fetchMovieGenre = async () => {
     }
     const data = await response.json();
     const limitedMovies = data.results.slice(0, 6);
-    console.log(limitedMovies);
     const movieTitleList = limitedMovies.map((data) => data.title);
     const movieCategoryIdList = limitedMovies.map((data) => data.genre_ids[0]);
     const movieBackdropUrlList = limitedMovies.map(
@@ -44,8 +36,6 @@ const fetchMovieGenre = async () => {
       }
       return movieCategoryNameList;
     };
-    console.log(createCategoryNameList());
-    console.log(movieTitleList);
 
     const createCardContainer = () => {
       const movieContainer = document.getElementById("movie-container");
@@ -61,47 +51,23 @@ const fetchMovieGenre = async () => {
       }
     };
 
-    // const createImageContainer = (itemNumber) => {
-    //   const imageContainer =
-    //     document.getElementsByClassName("card col-sm")[itemNumber];
-    //   const imageContainDiv = document.createElement("div");
-    //   imageContainDiv.setAttribute("class", "setBackdrop");
-    //   imageContainDiv.innerHTML = "";
-    //   imageContainer.append(imageContainDiv);
-    // };
-
-    const createCard = (column, movieData) => {
+    const createCard = (column, item) => {
       const cardContainer =
         document.getElementsByClassName("card-container")[column];
       const cardList = document.createElement("div");
       cardList.setAttribute("class", "card col-sm", "style", "width: 18rem");
 
-      // createImageContainer(movieData);
-      createBackdrop(column, movieData);
-
       cardList.innerHTML = `
+            <div><img style="width:100%" src= https://image.tmdb.org/t/p/w200${
+              movieBackdropUrlList[item]
+            }></div>
             <div class="card-body">
-              <h5 class="card-title">${movieTitleList[movieData]}</h5>
-              <h5 class="card-text">${createCategoryNameList()[movieData]}</h5>
+              <h5 class="card-title">${movieTitleList[item]}</h5>
+              <h5 class="card-text">${createCategoryNameList()[item]}</h5>
               <a href="/detail.html" class="btn btn-primary">Go Detail page</a>
             </div>
             `;
       cardContainer.append(cardList);
-    };
-
-    const createBackdrop = (column, index) => {
-      const backdropUrl = `https://image.tmdb.org/t/p/w200${movieBackdropUrlList[index]}`;
-      const backdrop = document.createElement("IMG");
-      // backdrop.setAttribute("class", `card col-sm`);
-      backdrop.setAttribute("src", `${backdropUrl}`);
-      // backdrop.setAttribute("width", "200");
-      // backdrop.setAttribute("height", "200");
-      backdrop.setAttribute("alt", `${movieTitleList[index]}`);
-      // document.getElementsByClassName("setBackdrop")[column].append(backdrop);
-      document
-        .getElementsByClassName("card-container")
-        [column].append(backdrop);
-      // movie1Image.innerHTML = `<img class="card-img-top" id="movie1Image" src=https://image.tmdb.org/t/p/w500/hZkgoQYus5vegHoetLkCJzb17zJ.jpg alt="Card image cap">`;
     };
 
     const createCardList = (row, column) => {
@@ -109,291 +75,12 @@ const fetchMovieGenre = async () => {
       for (let t = 0; t < column; t++) {
         for (let i = 0; i < row; i++) {
           createCard(t, i + 3 * t);
-          // createImageContainer();
-          // createBackdrop(t, i + 3 * t);
         }
       }
     };
-
     createCardList(3, 2);
-    //     });
-    //   });
-    // };
-
-    //       //       const movie1Title = document.getElementById("movie1Title");
-    //       //       const movie1Category = document.getElementById("movie1Category");
-    //       //       const backdrop_path = data.backdrop_path;
-    //       //       const backdropUrl = `https://image.tmdb.org/t/p/w200${backdrop_path}`;
-    //       //       console.log(backdropUrl);
-
-    //       //       movie1Title.innerHTML = `${data.title}`;
-    //       //       movie1Category.innerHTML = `${data.genres[0].name}`;
-
-    //       //       const createBackdrop = () => {
-    //       //         const backdrop = document.createElement("IMG");
-    //       //         backdrop.setAttribute("src", `${backdropUrl}`);
-    //       //         // backdrop.setAttribute("width", "200");
-    //       //         // backdrop.setAttribute("height", "200");
-    //       //         backdrop.setAttribute("alt", "fight club");
-    //       //         document.getElementById("setBackdrop").append(backdrop);
-    //       //         // movie1Image.innerHTML = `<img class="card-img-top" id="movie1Image" src=https://image.tmdb.org/t/p/w500/hZkgoQYus5vegHoetLkCJzb17zJ.jpg alt="Card image cap">`;
-    //       //         console.log(data);
-    //       //       };
-    //       //       createBackdrop();
-
-    // const fetchMovie = () => {
-    //   fetch(
-    //     `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`
-    //   ).then((response) => {
-    //     if (response.status == 200) {
-    //       console.log("It's working!");
-    //     } else if (response.status == 404) {
-    //       console.log("Something wrong, try again");
-    //     }
-    //     response.json().then((data) => {
-    //       console.log(data);
-    //       // console.log(data.results);
-    //       // const limitedMovies = data.results.slice(0, 6);
-    //       // console.log(limitedMovies);
-    //       // const movieTitleList = limitedMovies.map((data) => data.title);
-    //       // const movieCategoryList = limitedMovies.map((data) => data.category);
-    //       // console.log(movieCategoryList);
-
-    //       const createCardContainer = () => {
-    //         const movieContainer = document.getElementById("movie-container");
-    //         const cardContainer = document.createElement("div");
-    //         cardContainer.setAttribute("class", "row card-container");
-    //         cardContainer.innerHTML = "";
-    //         movieContainer.append(cardContainer);
-    //       };
-
-    //       const setCreateCardContainer = (column) => {
-    //         for (let i = 0; i < column; i++) {
-    //           createCardContainer();
-    //         }
-    //       };
-
-    //       const createCard = (i) => {
-    //         const cardContainer =
-    //           document.getElementsByClassName("card-container")[i];
-    //         const cardList = document.createElement("div");
-    //         cardList.setAttribute("class", "card col-sm", "style", "width: 18rem");
-    //         cardList.innerHTML = `
-    //         <div class="card-body">
-    //           <h5 class="card-title">Movie1</h5>
-    //           <h5 class="card-text">Category1</h5>
-    //           <a href="/detail.html" class="btn btn-primary">Go Detail page</a>
-    //         </div>
-    //         `;
-    //         cardContainer.append(cardList);
-    //       };
-
-    //       const createCardList = (row, column) => {
-    //         setCreateCardContainer(column);
-    //         for (let t = 0; t < column; t++) {
-    //           for (let i = 0; i < row; i++) {
-    //             createCard(t);
-    //           }
-    //         }
-    //       };
-
-    //       createCardList(3, 2);
-
-    //       //       const movie1Title = document.getElementById("movie1Title");
-    //       //       const movie1Category = document.getElementById("movie1Category");
-    //       //       const backdrop_path = data.backdrop_path;
-    //       //       const backdropUrl = `https://image.tmdb.org/t/p/w200${backdrop_path}`;
-    //       //       console.log(backdropUrl);
-
-    //       //       movie1Title.innerHTML = `${data.title}`;
-    //       //       movie1Category.innerHTML = `${data.genres[0].name}`;
-
-    //       //       const createBackdrop = () => {
-    //       //         const backdrop = document.createElement("IMG");
-    //       //         backdrop.setAttribute("src", `${backdropUrl}`);
-    //       //         // backdrop.setAttribute("width", "200");
-    //       //         // backdrop.setAttribute("height", "200");
-    //       //         backdrop.setAttribute("alt", "fight club");
-    //       //         document.getElementById("setBackdrop").append(backdrop);
-    //       //         // movie1Image.innerHTML = `<img class="card-img-top" id="movie1Image" src=https://image.tmdb.org/t/p/w500/hZkgoQYus5vegHoetLkCJzb17zJ.jpg alt="Card image cap">`;
-    //       //         console.log(data);
-    //       //       };
-    //       //       createBackdrop();
-    //     });
-    //   });
   };
   fetchMovie();
 };
 
 fetchMovieGenre();
-// const fetchMovie = async () => {
-//   const response = await fetch(
-//     `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`
-//   );
-//   if (response.status == 200) {
-//     console.log("It's working!");
-//   } else if (response.status == 404) {
-//     console.log("Something wrong, try again");
-//   }
-//   const data = await response.json();
-//   console.log(data);
-//   //       // console.log(data.results);
-//   //       // const limitedMovies = data.results.slice(0, 6);
-//   //       // console.log(limitedMovies);
-//   //       // const movieTitleList = limitedMovies.map((data) => data.title);
-//   //       // const movieCategoryList = limitedMovies.map((data) => data.category);
-//   //       // console.log(movieCategoryList);
-
-//   //       const createCardContainer = () => {
-//   //         const movieContainer = document.getElementById("movie-container");
-//   //         const cardContainer = document.createElement("div");
-//   //         cardContainer.setAttribute("class", "row card-container");
-//   //         cardContainer.innerHTML = "";
-//   //         movieContainer.append(cardContainer);
-//   //       };
-
-//   //       const setCreateCardContainer = (column) => {
-//   //         for (let i = 0; i < column; i++) {
-//   //           createCardContainer();
-//   //         }
-//   //       };
-
-//   //       const createCard = (i) => {
-//   //         const cardContainer =
-//   //           document.getElementsByClassName("card-container")[i];
-//   //         const cardList = document.createElement("div");
-//   //         cardList.setAttribute("class", "card col-sm", "style", "width: 18rem");
-//   //         cardList.innerHTML = `
-//   //         <div class="card-body">
-//   //           <h5 class="card-title">Movie1</h5>
-//   //           <h5 class="card-text">Category1</h5>
-//   //           <a href="/detail.html" class="btn btn-primary">Go Detail page</a>
-//   //         </div>
-//   //         `;
-//   //         cardContainer.append(cardList);
-//   //       };
-
-//   //       const createCardList = (row, column) => {
-//   //         setCreateCardContainer(column);
-//   //         for (let t = 0; t < column; t++) {
-//   //           for (let i = 0; i < row; i++) {
-//   //             createCard(t);
-//   //           }
-//   //         }
-//   //       };
-
-//   //       createCardList(3, 2);
-
-//   //       //       const movie1Title = document.getElementById("movie1Title");
-//   //       //       const movie1Category = document.getElementById("movie1Category");
-//   //       //       const backdrop_path = data.backdrop_path;
-//   //       //       const backdropUrl = `https://image.tmdb.org/t/p/w200${backdrop_path}`;
-//   //       //       console.log(backdropUrl);
-
-//   //       //       movie1Title.innerHTML = `${data.title}`;
-//   //       //       movie1Category.innerHTML = `${data.genres[0].name}`;
-
-//   //       //       const createBackdrop = () => {
-//   //       //         const backdrop = document.createElement("IMG");
-//   //       //         backdrop.setAttribute("src", `${backdropUrl}`);
-//   //       //         // backdrop.setAttribute("width", "200");
-//   //       //         // backdrop.setAttribute("height", "200");
-//   //       //         backdrop.setAttribute("alt", "fight club");
-//   //       //         document.getElementById("setBackdrop").append(backdrop);
-//   //       //         // movie1Image.innerHTML = `<img class="card-img-top" id="movie1Image" src=https://image.tmdb.org/t/p/w500/hZkgoQYus5vegHoetLkCJzb17zJ.jpg alt="Card image cap">`;
-//   //       //         console.log(data);
-//   //       //       };
-//   //       //       createBackdrop();
-//   //     });
-//   //   });
-//   // };
-
-//   // const fetchMovie = () => {
-//   //   fetch(
-//   //     `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`
-//   //   ).then((response) => {
-//   //     if (response.status == 200) {
-//   //       console.log("It's working!");
-//   //     } else if (response.status == 404) {
-//   //       console.log("Something wrong, try again");
-//   //     }
-//   //     response.json().then((data) => {
-//   //       console.log(data);
-//   //       // console.log(data.results);
-//   //       // const limitedMovies = data.results.slice(0, 6);
-//   //       // console.log(limitedMovies);
-//   //       // const movieTitleList = limitedMovies.map((data) => data.title);
-//   //       // const movieCategoryList = limitedMovies.map((data) => data.category);
-//   //       // console.log(movieCategoryList);
-
-//   //       const createCardContainer = () => {
-//   //         const movieContainer = document.getElementById("movie-container");
-//   //         const cardContainer = document.createElement("div");
-//   //         cardContainer.setAttribute("class", "row card-container");
-//   //         cardContainer.innerHTML = "";
-//   //         movieContainer.append(cardContainer);
-//   //       };
-
-//   //       const setCreateCardContainer = (column) => {
-//   //         for (let i = 0; i < column; i++) {
-//   //           createCardContainer();
-//   //         }
-//   //       };
-
-//   //       const createCard = (i) => {
-//   //         const cardContainer =
-//   //           document.getElementsByClassName("card-container")[i];
-//   //         const cardList = document.createElement("div");
-//   //         cardList.setAttribute("class", "card col-sm", "style", "width: 18rem");
-//   //         cardList.innerHTML = `
-//   //         <div class="card-body">
-//   //           <h5 class="card-title">Movie1</h5>
-//   //           <h5 class="card-text">Category1</h5>
-//   //           <a href="/detail.html" class="btn btn-primary">Go Detail page</a>
-//   //         </div>
-//   //         `;
-//   //         cardContainer.append(cardList);
-//   //       };
-
-//   //       const createCardList = (row, column) => {
-//   //         setCreateCardContainer(column);
-//   //         for (let t = 0; t < column; t++) {
-//   //           for (let i = 0; i < row; i++) {
-//   //             createCard(t);
-//   //           }
-//   //         }
-//   //       };
-
-//   //       createCardList(3, 2);
-
-//   //       //       const movie1Title = document.getElementById("movie1Title");
-//   //       //       const movie1Category = document.getElementById("movie1Category");
-//   //       //       const backdrop_path = data.backdrop_path;
-//   //       //       const backdropUrl = `https://image.tmdb.org/t/p/w200${backdrop_path}`;
-//   //       //       console.log(backdropUrl);
-
-//   //       //       movie1Title.innerHTML = `${data.title}`;
-//   //       //       movie1Category.innerHTML = `${data.genres[0].name}`;
-
-//   //       //       const createBackdrop = () => {
-//   //       //         const backdrop = document.createElement("IMG");
-//   //       //         backdrop.setAttribute("src", `${backdropUrl}`);
-//   //       //         // backdrop.setAttribute("width", "200");
-//   //       //         // backdrop.setAttribute("height", "200");
-//   //       //         backdrop.setAttribute("alt", "fight club");
-//   //       //         document.getElementById("setBackdrop").append(backdrop);
-//   //       //         // movie1Image.innerHTML = `<img class="card-img-top" id="movie1Image" src=https://image.tmdb.org/t/p/w500/hZkgoQYus5vegHoetLkCJzb17zJ.jpg alt="Card image cap">`;
-//   //       //         console.log(data);
-//   //       //       };
-//   //       //       createBackdrop();
-//   //     });
-//   //   });
-// };
-
-// fetchMovie();
-
-// https://image.tmdb.org/t/p/w500${data.backdrop_path}
-
-// https://image.tmdb.org/t/p/w500/hZkgoQYus5vegHoetLkCJzb17zJ.jpg
-// 157336
-//`https://api.themoviedb.org/3/movie/550?api_key=${apiKey}`
