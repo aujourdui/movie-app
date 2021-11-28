d9bf2308e5852c73554b408b266c8b5d0d9fc18d
kyosuke ito <kyosuke.ito@gmail.com>
2021-11-28 10:22:17 -0800

    create stable ver

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ const fetchMovieGenre = async (url) => {
   return genres;
 };
 
+// `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=
+// //"movie"&poster_path="string"`
+
 const fetchMovie = async (url) => {
   const response = await fetch(url);
   if (response.status == 200) {
@@ -25,28 +28,42 @@ const fetchMovie = async (url) => {
   }
   const data = await response.json();
   const result = data.results;
-
-  const title = await result.map((data) => data.title);
-  const categoryIdList = await result.map((data) => data.genre_ids[0]);
-  const backdropUrl = await result.map((data) => data.backdrop_path);
-  const overview = await result.map((data) => data.overview);
-  const releaseDate = await result.map((data) => data.release_date);
-
+  const movieTitleList = result.map((data) => data.title);
+  const movieCategoryIdList = result.map((data) => data.genre_ids[0]);
+  const movieBackdropUrlList = result.map((data) => data.backdrop_path);
+  const movieOverviewList = result.map((data) => data.overview);
+  const movieReleaseDateList = result.map((data) => data.release_date);
   const genres = await fetchMovieGenre(genreUrl);
 
   const createCategoryNameList = () => {
-    const categoryNameList = [];
-    for (let i = 0; i < categoryIdList.length; i++) {
+    const movieCategoryNameList = [];
+    for (let i = 0; i < movieCategoryIdList.length; i++) {
       for (let t = 0; t < genres.length; t++) {
-        if (categoryIdList[i] == genres[t].id) {
-          categoryNameList.push(genres[t].name);
+        if (movieCategoryIdList[i] == genres[t].id) {
+          movieCategoryNameList.push(genres[t].name);
         }
       }
     }
-    return categoryNameList;
+    return movieCategoryNameList;
   };
-  const categoryName = createCategoryNameList();
+  const movieCategoryNameList = createCategoryNameList();
 
+  setData(
+    movieTitleList,
+    movieCategoryNameList,
+    movieBackdropUrlList,
+    movieOverviewList,
+    movieReleaseDateList
+  );
+};
+
+const setData = (
+  movieTitleList,
+  movieCategoryNameList,
+  movieBackdropUrlList,
+  movieOverviewList,
+  movieReleaseDateList
+) => {
   const createCardContainer = () => {
     const movieContainer = document.getElementById("movie-container");
     const cardContainer = document.createElement("div");
@@ -60,82 +77,64 @@ const fetchMovie = async (url) => {
     }
   };
 
-  const createCard = (column, itemIndex) => {
+  const createCard = (column, item) => {
     const cardContainer =
       document.getElementsByClassName("card-container")[column];
     const cardList = document.createElement("div");
     cardList.setAttribute("class", "card col-sm", "style", "width: 18rem");
 
     cardList.innerHTML = `
-  <div><img style="width:100%" src= ${imgUrl}${backdropUrl[itemIndex]}></div>
+  <div><img style="width:100%" src= https://image.tmdb.org/t/p/w500${movieBackdropUrlList[item]}></div>
   <div class="card-body">
-    <h2 class="card-title">${title[itemIndex]}</h2>
-    <h3 class="card-text">${categoryName[itemIndex]}</h3>
-    <a href="detail.html" id=${itemIndex} onclick="window.localStorage.setItem('btnId', JSON.stringify(this.id))"  button type="button" class="btn btn-info">Go Detail page</a>
+    <h2 class="card-title">${movieTitleList[item]}</h2>
+    <h3 class="card-text">${movieCategoryNameList[item]}</h3>
+    <a href="detail.html" id=${item} onclick="window.localStorage.setItem('btnId', JSON.stringify(this.id))"  button type="button" class="btn btn-info">Go Detail page</a>
   </div>
   `;
     cardContainer.append(cardList);
+  };
 
-    if (window.location.href == "http://127.0.0.1:5500/index.html") {
-      const createCardList = (row, column) => {
-        setCreateCardContainer(column);
-        for (let t = 0; t < column; t++) {
-          for (let i = 0; i < row; i++) {
-            createCard(t, i + row * t);
-          }
+  if (window.location.href == "http://127.0.0.1:5500/index.html") {
+    const createCardList = (row, column) => {
+      setCreateCardContainer(column);
+      for (let t = 0; t < column; t++) {
+        for (let i = 0; i < row; i++) {
+          createCard(t, i + row * t);
         }
-      };
-      createCardList(4, 5);
-    } else {
-      // detail page
-      const createDetailCard = (itemIndex) => {
-        const detail = document.getElementById("movie-container-detail");
-        const createDetailContainer = document.createElement("div");
-        createDetailContainer.setAttribute(
-          "class",
-          "card col-md-8",
-          "style",
-          "width: 100%"
-        );
-        createDetailContainer.innerHTML = `
-    <div><img style="width:100%" src= https://image.tmdb.org/t/p/w500${backdropUrl[itemIndex]}></div>
+      }
+    };
+    createCardList(4, 5);
+  } else {
+    // detail page
+    const createDetailCard = (item) => {
+      const detail = document.getElementById("movie-container-detail");
+      detail.innerHTML = "";
+      const createDetailContainer = document.createElement("div");
+      createDetailContainer.setAttribute(
+        "class",
+        "card col-md-8",
+        "style",
+        "width: 100%"
+      );
+      createDetailContainer.innerHTML = `
+    <div><img style="width:100%" src= https://image.tmdb.org/t/p/w500${
+      movieBackdropUrlList[item]
+    }></div>
     <div class="card-body">
-      <h1 class="card-title">${title[itemIndex]}</h1>
-      <h2 class="card-text">${categoryName[itemIndex]}</h2>
-      <h4>${releaseDate[itemIndex]}</h4>
-      <p>${overview[itemIndex]}</p>
+      <h1 class="card-title">${movieTitleList[item]}</h1>
+      <h2 class="card-text">${createCategoryNameList()[item]}</h2>
+      <h4>${movieReleaseDateList[item]}</h4>
+      <p>${movieOverviewList[item]}</p>
       <a href="index.html" button type="button" class="btn btn-primary">Return Home page</a>
     </div>
     `;
-        detail.append(createDetailContainer);
-      };
+      detail.append(createDetailContainer);
+    };
 
-      const btnIdStr = JSON.parse(window.localStorage.getItem("btnId"));
-      const btnId = parseInt(btnIdStr);
-      createDetailCard(btnId);
-    }
-    // createDataList(result);
-  };
-
-  // const createDataList = async (result) => {
-
-  // };
-
-  // setContents(
-  //   titleList,
-  //   categoryNameList,
-  //   backdropUrlList,
-  //   overviewList,
-  //   releaseDateList
-  // );
+    const btnIdStr = JSON.parse(window.localStorage.getItem("btnId"));
+    const btnId = parseInt(btnIdStr);
+    createDetailCard(btnId);
+  }
 };
-fetchMovie(searchUrl);
 
-// const setContents = async (
-//   title,
-//   categoryName,
-//   backdropUrl,
-//   overview,
-//   releaseDate
-// ) => {
-// };
+fetchMovie(searchUrl);
