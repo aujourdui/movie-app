ea7d62e0ceb3a9946d9b89635ad1a74e4e4de1c6
kyosuke ito <kyosuke.ito@gmail.com>
2021-12-17 09:33:42 +0900

    change structure different way

--- script.js
+++ script.js
@@ -1,13 +1,74 @@
-const setData = (
-  title,
-  categoryName,
-  backdropUrl,
-  posterUrl,
-  overview,
-  releaseDate,
-  vote,
-  url
-) => {
+const apiKey = "api_key=67a465f165043b63372ea02407bc5582";
+const baseUrl = "https://api.themoviedb.org/3";
+const imgUrl = "https://image.tmdb.org/t/p/w500";
+
+const popularUrl = `${baseUrl}/discover/movie?sort_by=popularity.desc&${apiKey}`;
+const genreUrl = `${baseUrl}/genre/movie/list?${apiKey}`;
+const searchUrl = `${baseUrl}/search/movie?${apiKey}&query=`;
+
+const movieContainer = document.getElementById("movie-container");
+const cardContainer = document.createElement("div");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+// home page
+const fetchMovieGenre = async (url) => {
+  const response = await fetch(url);
+  if (response.status == 200) {
+    console.log("It's working!");
+  } else if (response.status == 404) {
+    console.log("Something wrong, try again");
+  }
+  const data = await response.json();
+  const genres = data.genres;
+  return genres;
+};
+
+const fetchMovie = async (url) => {
+  const response = await fetch(url);
+  if (response.status == 200) {
+    console.log("It's working!");
+  } else if (response.status == 404) {
+    console.log("Something wrong, try again");
+  }
+  const data = await response.json();
+  const result = data.results;
+  // console.log(result);
+  displayMovie(result, url);
+};
+
+const displayMovie = async (result, url) => {
+  const titleList = result.map((data) => data.title);
+  const categoryIdList = result.map((data) => data.genre_ids[0]);
+  const backdropUrlList = result.map((data) => data.backdrop_path);
+  const posterUrlList = result.map((data) => data.poster_path);
+  const overviewList = result.map((data) => data.overview);
+  const releaseDateList = result.map((data) => data.release_date);
+  const voteList = result.map((data) => data.vote_average);
+  const genres = await fetchMovieGenre(genreUrl);
+  const categoryNameList = createCategoryNameList(categoryIdList, genres);
+
+  if (url !== popularUrl) {
+    window.localStorage.setItem("newTitleList", JSON.stringify(`${titleList}`));
+    window.localStorage.setItem(
+      "newCategoryNameList",
+      JSON.stringify(`${categoryNameList}`)
+    );
+    window.localStorage.setItem(
+      "newPosterUrlList",
+      JSON.stringify(`${posterUrlList}`)
+    );
+    window.localStorage.setItem(
+      "newOverviewList",
+      JSON.stringify(`${overviewList}`)
+    );
+    window.localStorage.setItem(
+      "newReleaseDateList",
+      JSON.stringify(`${releaseDateList}`)
+    );
+    window.localStorage.setItem("url", JSON.stringify(url));
+  }
+
   const createCardContainer = () => {
     const movieContainer = document.getElementById("movie-container");
     const cardContainer = document.createElement("div");
@@ -34,21 +95,20 @@ const setData = (
         window.localStorage.setItem("btnId", JSON.stringify(`${itemIndex}`));
       },
     });
-    console.log(overview);
-    const shortOverviewList = overview.map((overview) =>
+    const shortOverviewList = overviewList.map((overview) =>
       overview.length >= 50 ? overview.substring(0, 50) + " ..." : overview
     );
 
     cardList.innerHTML = `
     <div><img style="width:100%" src= ${imgUrl}${
-      backdropUrl[itemIndex]
-    } onerror="imgError(this)"></div>
+      backdropUrlList[itemIndex]
+    } onerror="this.onerror=null;this.src='/public/images/error-image.jpeg'"></div>
     <div class="card-body">
-      <h2 class="card-title">${title[itemIndex]}</h2>
-      <h3 class="card-text">${categoryName[itemIndex]}</h3>
+      <h2 class="card-title">${titleList[itemIndex]}</h2>
+      <h3 class="card-text">${categoryNameList[itemIndex]}</h3>
       <p class="hide">${shortOverviewList[itemIndex]}</p>
-      <p class="hide">${releaseDate[itemIndex]}</p>
-      <span class=${getColor(vote[itemIndex])}>${vote[itemIndex]}</span>
+      <p class="hide">${releaseDateList[itemIndex]}</p>
+      <span class=${getColor(voteList[itemIndex])}>${voteList[itemIndex]}</span>
     </div>
   `;
     cardContainer.append(cardList);
@@ -95,15 +155,15 @@ const setData = (
           "width: 100%"
         );
         createDetailContainer.innerHTML = `
-        <div><img style="width:100%" src= ${imgUrl}${posterUrl[itemIndex]} onerror="imgErrorDetail(this)"}></div>
-        <div class="card-body">
-          <h1 class="card-title">${title[itemIndex]}</h1>
-          <h2 class="card-text">${categoryName[itemIndex]}</h2>
-          <h4>${releaseDate[itemIndex]}</h4>
-        <p>${overview[itemIndex]}</p>
-        <a href="index.html" button type="button" class="btn btn-primary">Return to Home</a>
-        </div>
-    `;
+            <div><img style="width:100%" src= ${imgUrl}${posterUrlList[itemIndex]} onerror="this.onerror=null;this.src='/public/images/error-image-detail.png'"}></div>
+            <div class="card-body">
+              <h1 class="card-title">${titleList[itemIndex]}</h1>
+              <h2 class="card-text">${categoryNameList[itemIndex]}</h2>
+              <h4>${releaseDateList[itemIndex]}</h4>
+            <p>${overviewList[itemIndex]}</p>
+            <a href="index.html" button type="button" class="btn btn-primary">Return to Home</a>
+            </div>
+        `;
         detail.append(createDetailContainer);
       };
 
@@ -128,15 +188,15 @@ const setData = (
           "width: 100%"
         );
         createDetailContainer.innerHTML = `
-        <div><img style="width:100%" src= ${imgUrl}${posterUrl[itemIndex]} onerror="imgErrorDetail(this)"></div>
-        <div class="card-body">
-          <h1 class="card-title">${title[itemIndex]}</h1>
-          <h2 class="card-text">${categoryName[itemIndex]}</h2>
-          <h4>${releaseDate[itemIndex]}</h4>
-        <p>${overview[itemIndex]}</p>
-        <a href="index.html" button type="button" class="btn btn-primary">Return to Home</a>
-        </div>
-    `;
+            <div><img style="width:100%" src= ${imgUrl}${posterUrl[itemIndex]} onerror="this.onerror=null;this.src='/public/images/error-image-detail.png'"></div>
+            <div class="card-body">
+              <h1 class="card-title">${title[itemIndex]}</h1>
+              <h2 class="card-text">${categoryName[itemIndex]}</h2>
+              <h4>${releaseDate[itemIndex]}</h4>
+            <p>${overview[itemIndex]}</p>
+            <a href="index.html" button type="button" class="btn btn-primary">Return to Home</a>
+            </div>
+        `;
         detail.append(createDetailContainer);
       };
 
@@ -148,13 +208,21 @@ const setData = (
   }
 };
 
+const createCategoryNameList = (categoryIdList, genres) => {
+  const categoryNameList = [];
+  for (let i = 0; i < categoryIdList.length; i++) {
+    for (let t = 0; t < genres.length; t++) {
+      if (categoryIdList[i] == genres[t].id) {
+        categoryNameList.push(genres[t].name);
+      }
+    }
+  }
+  return categoryNameList;
+};
+
 fetchMovie(popularUrl);
 
 const searchByQuery = () => {
-  const form = document.getElementById("form");
-  const search = document.getElementById("search");
-  const movieContainer = document.getElementById("movie-container");
-
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -169,15 +237,9 @@ const searchByQuery = () => {
   });
 };
 
-const imgError = (image) => {
-  image.onerror = "";
-  image.src = "/public/images/error-image.jpeg";
-  return true;
-};
-
 const imgErrorDetail = (image) => {
   image.onerror = "";
-  image.src = "/public/images/error-image-detail.png";
+  image.src = "/public/images/error-image-detail.jpeg";
   return true;
 };
 
